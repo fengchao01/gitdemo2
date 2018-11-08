96ba7512a5aea96f4d11d4778cfcdf22bfac0d3d
fc <fengchao@fc.com>
2018-11-08 09:07:09 +0800

    Initial commit

---
+++ src/main/java/com/fc/fc/GitdemoApplication.java
@@ -0,0 +1,12 @@
+package com.fc.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitdemoApplication.class, args);
+    }
+}
