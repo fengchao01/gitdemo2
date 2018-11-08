679138088aaf0e3564d37dcb7fcb77aa2326e16c
fc <fengchao@fc.com>
2018-11-08 09:13:46 +0800

    修改包名提交

--- src/main/java/com/fc/ServletInitializer.java
+++ src/main/java/com/fc/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.fc.fc;
+package com.fc;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
